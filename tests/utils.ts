18e613345080097298784a6fe90dfb492361ef40
Egor Ivanov <cheshi.mantu@pm.me>
2022-09-27 11:02:08 +0300

    added various errors

--- tests/utils.ts
+++ tests/utils.ts
@@ -10,8 +10,16 @@ export const attachMicroservice = (val: string) => {
 };
 
 const isTimeToThrow = () => {
-  if (Math.random() > 0.98) {
+  var failing:number = Math.random();
+
+  if (failing > 0.91 && failing <= 0.92) {
     throw new Error("net::ERR_CONNECTION_REFUSED");
+  }
+  if (failing >= 0.93 && failing <= 0.95) {
+    throw new Error("Element not found {selector: something}\n Expected: visible or transparent: visible or have css value opacity=0\n Timeout: 6000 ms");
+  }
+  if (failing >= 0.94 && failing <= 0.99) {
+    throw new Error("Test timeout of 30000ms exceeded.");
   }
 };
 
