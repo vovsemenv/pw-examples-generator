3fb1a29db50120d91272818e9f5d91fd79b5899b
Vladimir Semenov <vovsemenv@gmail.com>
2022-09-27 16:19:50 +0400

    change config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,4 @@
 import type { PlaywrightTestConfig } from "@playwright/test";
-import { devices } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
   testDir: "./tests",
@@ -7,7 +6,7 @@ const config: PlaywrightTestConfig = {
   expect: { timeout: 5000 },
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+  retries: 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: [["dot"], ["allure-playwright"]],
   use: {
@@ -16,9 +15,6 @@ const config: PlaywrightTestConfig = {
   projects: [
     {
       name: "chromium",
-      // use: {
-      //   ...devices["Desktop Chrome"],
-      // },
     },
   ],
 };
