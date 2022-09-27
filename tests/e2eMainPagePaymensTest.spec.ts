50086c1c772b3d88d8121d5ab7655e3b875241e3
Egor Ivanov <cheshi.mantu@pm.me>
2022-09-27 13:39:57 +0300

    fixes the meta-data absence in case of failures in fixtures

--- tests/e2eMainPagePaymensTest.spec.ts
+++ tests/e2eMainPagePaymensTest.spec.ts
@@ -1,35 +1,44 @@
 import { test } from "@playwright/test";
 import { allure } from "allure-playwright";
 import { notes } from "./notes";
-import { attachJiraIssue, attachMicroservice,   authorize,   createNewEntity, deleteNewEntity  } from "./utils";
+import {
+  attachJiraIssue,
+  attachMicroservice,
+  authorize,
+  createNewEntity,
+  deleteNewEntity,
+} from "./utils";
 
 test.describe("e2eManPageTests", () => {
   test.beforeEach(async () => {
     allure.label({ name: "layer", value: "e2e" });
     allure.owner("eroshenkoam");
     allure.feature("Payments widget on main page");
-    await authorize();
   });
 
   notes.forEach((noteName) => {
-    test(`End user should be able to create a new payment request: ${noteName}`, () => {
-      allure.tag("e2e", "critical", "regress", "release","regular", "nightly");
+    test(`End user should be able to create a new payment request: ${noteName}`, async () => {
+      allure.tag("e2e", "critical", "regress", "release", "regular", "nightly");
       attachJiraIssue("AD-2");
       allure.story("Manage payments via main page widget");
       attachMicroservice("Billing");
       allure.addParameter("Title", noteName);
       createNewEntity("payment");
+
+      await authorize();
     });
   });
 
   notes.forEach((noteName) => {
-    test(`End user should be able to cancel created payment request: ${noteName}`, () => {
+    test(`End user should be able to cancel created payment request: ${noteName}`, async () => {
       allure.tag("e2e", "regress", "critical", "release", "regular", "nightly");
       attachJiraIssue("AD-3");
       allure.story("Manage payments via main page widget");
       attachMicroservice("Billing");
       allure.addParameter("Title", noteName);
       deleteNewEntity("payment");
+
+      await authorize();
     });
   });
 });
--- tests/e2eSupportIssuesTest.spec.ts
+++ tests/e2eSupportIssuesTest.spec.ts
@@ -13,22 +13,23 @@ test.describe("IssuesWebTest", () => {
     allure.label({ name: "layer", value: "e2e" });
     allure.owner("eroshenkoam");
     allure.feature("Support requests handling");
-    await authorize();
   });
 
-  test("Authenticated user must be able to create a support request with type Issue", () => {
+  test("Authenticated user must be able to create a support request with type Issue", async () => {
     allure.tag("web", "critical", "regress");
     attachJiraIssue("AD-5");
     allure.story("Managing support requests with type Issue");
     attachMicroservice("Support");
     createNewEntity("issue");
+    await authorize();
   });
 
-  test("Authenticated user must be able to close existing support request with type Issue", () => {
+  test("Authenticated user must be able to close existing support request with type Issue", async () => {
     allure.tag("web", "regress");
     attachJiraIssue("AD-6");
     allure.story("Managing support requests with type Issue");
     attachMicroservice("Support");
     deleteNewEntity("issue");
+    await authorize();
   });
 });
